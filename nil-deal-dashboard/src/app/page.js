253cc148a8c4a348aaaf9b98a68957f65fe7fff3
Harry Chaklader <102170462+harrychaklader@users.noreply.github.com>
2023-09-16 16:29:22 -0400

    Added Header component. Next => Build web scraper.

--- nil-deal-dashboard/src/app/page.js
+++ nil-deal-dashboard/src/app/page.js
@@ -1,10 +1,12 @@
+import AppHero from "@/components/AppHero"
 import DealList from "@/components/DealList"
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="bg-gradient-to-br from-sky-50 to-sky-100 flex min-h-screen flex-col items-center justify-between p-24">
       <div>
+        <AppHero />
         <DealList />
       </div>
     </main>
---
+++ nil-deal-dashboard/src/components/AppHero.js
@@ -0,0 +1,10 @@
+const AppHero = () => {
+    return (
+        <><h1 className="font-sans text-6xl font-bold text-center text-blue-600 drop-shadow-md m-4">NIL Opportunity Dashboard</h1>
+           <h2 className="font-sans text-3xl text-center m-4">Empowering Student-Athletes Through Name, Image, and Likeness Opportunities</h2>
+           <p className="text-slate-700 text-center m-4">By providing a platform for student-athletes of all backgrounds to monetize their NIL rights, we are helping to level the playing field and create a more inclusive environment.</p>
+        </>
+    );
+};
+
+export default AppHero;
