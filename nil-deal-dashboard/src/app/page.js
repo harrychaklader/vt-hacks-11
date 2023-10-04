1ccf2559ccfe6de47dccd02237974e3f8934f8a9
Harry Chaklader <102170462+harrychaklader@users.noreply.github.com>
2023-10-04 15:51:22 -0400

    Updated Deals

--- nil-deal-dashboard/src/app/page.js
+++ nil-deal-dashboard/src/app/page.js
@@ -1,12 +1,14 @@
 import AppHeader from "@/components/AppHeader"
 import AppHero from "@/components/AppHero"
 import DealList from "@/components/DealList"
+import Sidebar from "@/components/SideBar"
 
 export default function Home() {
   return (
     <main className="bg-gradient-to-br from-sky-50 to-sky-100 flex min-h-screen flex-col items-center justify-between p-36">
       <div>
         <AppHeader />
+        <Sidebar />
         <AppHero />
         <DealList />
       </div>
